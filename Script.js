1bdc72a8bdc72f8bc62d59a41e11df1b5cac1400
AnusSlayer <dron.urec@mail.ru>
2021-01-28 00:07:42 +0300

    Secont commit

--- Script.js
+++ Script.js
@@ -1,6 +1,6 @@
 "use sctrict";
 
-let numberOfFilms = 0, film, bal, film2, bal2;
+let numberOfFilms;
 
 numberOfFilms = +prompt("Сколько фильмоф вы уже посмотрели", "");
 
@@ -12,12 +12,57 @@ let personalMovieDB = {
     private: false
 };
 
-film = prompt("Один из последних просмотренных фильмов?", "");
-bal = prompt("На сколько оцениваете его?", "");
-film2 = prompt("Один из последних просмотренных фильмов?", "");
-bal2 = prompt("На сколько оцениваете его?", "");
+if (personalMovieDB.count < 10) {
+    alert('Маловато смотришь');
+} else if (personalMovieDB.count < 30) {
+    alert('Вы классный зритель');
+    } else {
+        alert('Вы киноман!');
+        };
 
-personalMovieDB.movies[film] = bal;
-personalMovieDB.movies[film2] = bal2;
+// for ( let i=1, film, bal; i<=numberOfFilms; i++) {
+
+//     film = prompt ("Один из последних просмотренных фильмов?", "");
+
+//         if (film.length == 0) {
+//             alert('Наебать меня хочешь? Вводи фильм');
+//             i--;
+//             continue;
+//         } else  if (film.length > 50){
+//             alert('Наебать меня хочешь? Вводи фильм');
+//             i--;
+//             continue;
+//             } else{
+//     for (let i1=1, bal; i1 < 2; i1++) {
+
+//     bal = prompt("На сколько оцениваете его?", "");
+  
+//         if (bal.length == 0) {
+//             alert('Наебать меня хочешь? Вводи фильм');
+//             i1--;
+//             continue;
+//         }
+//         personalMovieDB.movies[film] = bal;
+//     }
+//             }
+// }
+
+for ( let i=1, film, bal; i<=numberOfFilms; i++) {
+    film = prompt ("Один из последних просмотренных фильмов?", "");
+
+        if (film != null && film.length != 0 && film.length < 50) {
+            bal = prompt('На сколько оцениваете его?', '');
+                if (bal != null && bal != 0){
+                personalMovieDB.movies[film] = bal;
+                } else {
+                    alert('Наебать меня хочешь? Пиши нормально');
+                    i--;
+                }
+        } else {
+            alert('Наебать меня хочешь? Вводи фильм');
+            i--;
+        }
+   
+}
 
 console.log(personalMovieDB);
